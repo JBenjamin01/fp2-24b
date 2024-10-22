65dc42de4bafb403815e60a531d7f46f42aafa2f
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-10-22 14:44:10 -0500

    Actividades del laboratorio 8

---
+++ Laboratorio/Práctica 08/Soldado.java
@@ -0,0 +1,45 @@
+public class Soldado {
+    private String nombre;
+    private int puntosVida;
+    private int fila;
+    private char columna;
+
+    public Soldado(String nombre, int puntosVida, int fila, char columna) {
+        this.nombre = nombre;
+        this.puntosVida = puntosVida;
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setPuntosVida(int puntosVida) {
+        this.puntosVida = puntosVida;
+    }
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+    public void setColumna(char columna) {
+        this.columna = columna;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getPuntosVida() {
+        return puntosVida;
+    }
+    public int getFila() {
+        return fila;
+    }
+    public char getColumna() {
+        return columna;
+    }
+    
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre 
+            + " | Puntos de Vida: " + puntosVida 
+            + " | Ubicación: " + fila + columna;
+    }
+}
---
+++ Laboratorio/Práctica 08/VideoJuego5.java
@@ -0,0 +1,181 @@
+// LABORATORIO N° 8 - EJERCICIO 1
+// AUTOR: JHONATAN BENJAMIN MAMANI CÉSPEDES
+// TIEMPO: 70 MINUTOS
+import java.util.*;
+public class VideoJuego5 {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Soldado>> tablero = new ArrayList<>();
+        HashMap<String, Soldado> e1 = new HashMap<>();
+        HashMap<String, Soldado> e2 = new HashMap<>();
+
+        inicializarTablero(tablero);
+        inicializarEjercitos(e1, e2, tablero);
+
+        mostrarTablero(tablero, e1, e2);
+
+        System.out.println("\nDatos del ejército 1:");
+        mostrarDatosEjercito(e1);
+        System.out.println("\nDatos del ejército 2:");
+        mostrarDatosEjercito(e2);
+
+        Soldado soldadoMayorVidaE1 = obtenerSoldadoMayorVida(e1);
+        Soldado soldadoMayorVidaE2 = obtenerSoldadoMayorVida(e2);
+    
+        System.out.println("\nSoldado con mayor vida del ejército 1:\n" + soldadoMayorVidaE1);
+        System.out.println("\nSoldado con mayor vida del ejército 2:\n" + soldadoMayorVidaE2);
+
+        double promedioVidaE1 = calcularPromedioVida(e1);
+        double promedioVidaE2 = calcularPromedioVida(e2);
+
+        System.out.println("\nPromedio de puntos de vida del ejército 1: " + promedioVidaE1);
+        System.out.println("Promedio de puntos de vida del ejército 2: " + promedioVidaE2);
+
+        ordenamientoInsertionSort(e1);
+        System.out.println("\nRanking de soldados del ejército 1 (ordenado por puntos de vida de forma decreciente):");
+        mostrarDatosEjercito(e1);
+
+
+        ordenamientoBubbleSort(e2);
+        System.out.println("\nRanking de soldados del ejército 2 (ordenado por puntos de vida de forma decreciente):");
+        mostrarDatosEjercito(e2);
+
+
+        String resultadoBatalla = determinarGanador(e1, e2);
+        System.out.println("\nResultado de la batalla:");
+        System.out.println(resultadoBatalla);
+    }
+
+    public static void inicializarTablero(ArrayList<ArrayList<Soldado>> tablero) {
+        for (int i = 0; i < 10; i++) {
+            ArrayList<Soldado> fila = new ArrayList<>();
+            for (int j = 0; j < 10; j++) 
+                fila.add(null);
+            tablero.add(fila);
+        }
+    }
+
+    public static void inicializarEjercitos(HashMap<String, Soldado> e1, HashMap<String, Soldado> e2,
+            ArrayList<ArrayList<Soldado>> tablero) {
+        for (int i = 0; i < 2; i++) {
+            int n = (int) (Math.random() * 10) + 1;
+            for (int j = 0; j < n; j++) {
+                int fila, columna;
+                do {
+                    fila = (int) (Math.random() * 10);
+                    columna = (int) (Math.random() * 10);
+                } while (tablero.get(fila).get(columna) != null); 
+
+                String nombre = "Soldado" + (i + 1) + "X" + (j + 1);
+                int puntosVida = (int) (Math.random() * 5) + 1;
+                Soldado soldado = new Soldado(nombre, puntosVida, fila + 1, (char) ('A' + columna));
+
+                if (i == 0)
+                    e1.put(nombre, soldado);
+                else
+                    e2.put(nombre, soldado);
+                tablero.get(fila).set(columna, soldado);
+            }
+        }
+    }
+
+    public static void mostrarTablero(ArrayList<ArrayList<Soldado>> tablero, HashMap<String, Soldado> e1, 
+                                        HashMap<String, Soldado> e2) {
+        System.out.println("Tablero de la batalla:"
+                            + "\nLas unidades del ejército 1 estarán con sus puntos de vida entre corchetes ([x])." 
+                            + "\nLas del ejército 2 con sus puntos de vida entre signos menor y mayor que (<x>):");
+        System.out.println("\n           A     B     C     D     E     F     G     H     I     J");
+        System.out.println();
+        System.out.println("        -------------------------------------------------------------");
+
+        for (int i = 0; i < tablero.size(); i++) {
+            System.out.print(i + 1 + "\t| ");
+            for (int j = 0; j < tablero.get(i).size(); j++) {
+                Soldado soldado = tablero.get(i).get(j);
+                if (soldado == null) {
+                    System.out.print("    | ");
+                } else {
+                    if (e1.containsKey(soldado.getNombre())) {
+                        System.out.print("[" + soldado.getPuntosVida() + "] | ");
+                    } else if (e2.containsKey(soldado.getNombre())) {
+                        System.out.print("<" + soldado.getPuntosVida() + "> | ");
+                    }
+                }
+            }
+            System.out.println("\n        -------------------------------------------------------------");
+        }
+    }
+
+    public static void mostrarDatosEjercito(HashMap<String, Soldado> ejercito) {
+        for (Soldado s : ejercito.values())
+            System.out.println(s);
+    }
+
+    public static Soldado obtenerSoldadoMayorVida(HashMap<String, Soldado> ejercito) {
+        Soldado mayorVida = null;
+        int maxPuntosVida = Integer.MIN_VALUE;
+        for (Soldado soldado : ejercito.values()) 
+            if (soldado.getPuntosVida() > maxPuntosVida) {
+                maxPuntosVida = soldado.getPuntosVida();
+                mayorVida = soldado;
+            }
+        return mayorVida;
+    }
+
+    public static double calcularPromedioVida(HashMap<String, Soldado> ejercito) {
+        double total = 0;
+        for (Soldado soldado : ejercito.values()) 
+            total += soldado.getPuntosVida();
+        return total / ejercito.size();
+    }
+
+    public static void ordenamientoInsertionSort(HashMap<String, Soldado> ejercito) {
+        List<String> keys = new ArrayList<>(ejercito.keySet());
+        for (int i = 1; i < keys.size(); i++) {
+            String key = keys.get(i);
+            Soldado soldadoActual = ejercito.get(key);
+            int j = i - 1;
+
+            while (j >= 0 && ejercito.get(keys.get(j)).getPuntosVida() < soldadoActual.getPuntosVida()) {
+                ejercito.put(keys.get(j + 1), ejercito.get(keys.get(j)));
+                j--;
+            }
+            ejercito.put(keys.get(j + 1), soldadoActual);
+        }
+    }
+    public static void ordenamientoBubbleSort(HashMap<String, Soldado> ejercito) {
+        List<String> keys = new ArrayList<>(ejercito.keySet());
+        boolean swapped;
+        int n = keys.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) 
+                if (ejercito.get(keys.get(j)).getPuntosVida() < ejercito.get(keys.get(j + 1)).getPuntosVida()) {
+                    Soldado temp = ejercito.get(keys.get(j));
+                    ejercito.put(keys.get(j), ejercito.get(keys.get(j + 1)));
+                    ejercito.put(keys.get(j + 1), temp);
+                    swapped = true;
+                }
+            if (!swapped) break;
+        }
+    }
+
+    public static String determinarGanador(HashMap<String, Soldado> e1, HashMap<String, Soldado> e2) {
+        int puntosE1 = 0;
+        for (Soldado soldado : e1.values()) 
+            puntosE1 += soldado.getPuntosVida();
+
+        int puntosE2 = 0;
+        for (Soldado soldado : e2.values()) 
+            puntosE2 += soldado.getPuntosVida();
+
+        if (puntosE1 > puntosE2) 
+            return "El ejército 1 ha ganado la batalla. La suma total de sus puntos de vida es " + puntosE1 
+                    + " superando en " + (puntosE1 - puntosE2) + " puntos al ejército 2.";
+        else if (puntosE2 > puntosE1) 
+            return "El ejército 2 ha ganado la batalla. La suma total de sus puntos de vida es " + puntosE2 
+                    + " superando en " + (puntosE2 - puntosE1) + " puntos al ejército 1.";
+        else 
+            return "La batalla ha terminado en empate. Ambos ejércitos tienen " + puntosE1 + " puntos de vida en total.";
+    }
+}
